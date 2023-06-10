14ef709f45f0d04319fdb721e3d1486972a908d1
codergift <sidlock2@gmail.com>
2023-06-10 20:16:58 +0530

    save new change

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
+import "./ExpenseItem.css";
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <h2>Expense Item!</h2>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 200</p>
+      <div className="expense-item__description">
+        <h2>Food</h2>
+        <h2>Petrol</h2>
+        <h2>Movies</h2>
+        <div className="expense-item__price">
+        <p>Rs 10</p>
+        <p>Rs 100</p>
+        <p>Rs 200</p>
+        </div>
+      </div>
     </div>
   );
 }
