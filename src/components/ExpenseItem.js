427fd4c2d15bf121d136476258bca2a3fd1986b8
codergift <sidlock2@gmail.com>
2023-06-11 06:20:07 +0530

    new variable added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,9 +3,11 @@ function ExpenseItem() {
   const expenseDate= new Date();
   const expenseTitle='Car Insurance';
   const expensePrice= 589.00
+  const LocationOfExpenditure='Royal Auto, Patna';
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{expenseDate.toISOString()}</div> 
+      <h4>{LocationOfExpenditure}</h4> 
       <div className="expense-item__description">
         <h2> {expenseTitle}</h2>
       </div>
