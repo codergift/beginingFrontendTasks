ad0a5869873d748044f4d8f26e3281588d561b18
codergift <sidlock2@gmail.com>
2023-06-13 18:15:20 +0530

    Added click and delete button

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -3,26 +3,30 @@ import "./ExpenseItem.css";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
-const ExpenseItem=(props) =>{
-  
-  return (
+const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("Clicked!!!");
+  };
 
-    <Card className='expense-item'>
-    
-    <ExpenseDate 
-    date ={props.date}
-     />
-     
-    
-    <ExpenseDetails 
-    amount={props.amount}  
-    // location={props.location}   
-    title={props.title} 
-    />
-    
+  const deleteExpenseHandler = () => {
+    // Perform the delete expense operation here
+    console.log('Deleting expense...');
+    // You can manipulate the DOM or trigger an API call to delete the expense
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+
+      <ExpenseDetails
+        amount={props.amount}
+        // location={props.location}
+        title={props.title}
+      />
+      <button on onClick={clickHandler}>Change title</button>
+
+      <button onClick={deleteExpenseHandler}>Delete Expense</button>
     </Card>
-    
-    );
-  
-}
+  );
+};
 export default ExpenseItem;
