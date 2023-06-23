c2fcc9457ef988c9e89922b522decba10fc9320f
saurabh-r1 <sidlock2@gmail.com>
2023-06-23 13:57:41 +0530

    some modifications

--- src/App.js
+++ src/App.js
@@ -18,14 +18,14 @@ const App=()=> {
     },
     {
       id: 'e2',
-      title: 'Toilet Paper',
+      title: 'School Fee',
       amount: 91.0,
-      date: new Date(2023, 6, 8),
+      date: new Date(2023, 4, 8),
       location: 'Patna',
     },
     {
       id: 'e3',
-      title: 'new phone',
+      title: '',
       amount: 5990.0,
       date: new Date(2023, 6, 3),
       location: 'Amazone',
@@ -34,7 +34,7 @@ const App=()=> {
       id: 'e4',
       title: 'Chair',
       amount: 258.0,
-      date: new Date(2023, 6, 4),
+      date: new Date(2023, 3, 4),
       location: 'Patna',
     },
   ]);
@@ -43,38 +43,6 @@ const App=()=> {
     setExpenses((prevExpenses) => [...prevExpenses, expense]);
   };
 
-  // return (
-  //   <Card className='expense'>
-  //     <NewExpense />
-  //     <ExpenseItem
-  //       title={expenses[0].title}
-  //       amount={expenses[0].amount}
-  //       date={expenses[0].date}
-  //       location={expenses[0].location}
-  //     />
-
-  //     <ExpenseItem
-  //     title={expenses[1].title}
-  //     amount={expenses[1].amount}
-  //     date={expenses[1].date}
-  //     location={expenses[1].location}
-  //     />
-
-  //     <ExpenseItem
-  //     title={expenses[2].title}
-  //     amount={expenses[2].amount}
-  //     date={expenses[2].date}
-  //     location={expenses[2].location}
-  //     />
-
-  //     <ExpenseItem 
-  //      title={expenses[3].title}
-  //      amount={expenses[3].amount}
-  //      date={expenses[3].date}
-  //      location={expenses[3].location}
-  //      />
-  //   </Card>
-  // );
   return(
     <div>
       <NewExpense onAddExpanse={addExpenseHandler} />
--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,25 +1,15 @@
-import React, { useState} from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const[title,setTitle]=useState (props.title);
-
-  const clickHandler = () => {
-    setTitle('updated!');
-    console.log(title); 
-  };
-
-  
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
-      <ExpenseDetails amount={props.amount} title={title} />
-      
-      <button onClick={clickHandler}>Change title</button>
+      <ExpenseDetails amount={props.amount} title={props.title} />
       
     </Card>
   );
